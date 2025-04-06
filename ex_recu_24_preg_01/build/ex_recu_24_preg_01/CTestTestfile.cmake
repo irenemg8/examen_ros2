5e155c8fd7918f0d614ab4d70ed31364fa530ad8
Irene Medina García <imedgar@epsg.upv.es>
2025-04-06 22:18:59 +0200

    Cambios Irene |
    Ej 2 - Funciona el correr el gazebo y rviz al mismo tiempo, solo queda la posición inicial

---
+++ ex_recu_24_preg_01/build/ex_recu_24_preg_01/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/irene/examen/examen_ros2/ex_recu_24_preg_01
+# Build directory: /home/irene/examen/examen_ros2/ex_recu_24_preg_01/build/ex_recu_24_preg_01
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
--- ex_recu_24_preg_02/setup.py
+++ ex_recu_24_preg_02/setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup
+import os #incluir
+from glob import glob #incluir
 
-package_name = 'ex_recu_24_preg_02'
+package_name = 'ex_recu_24_preg_02' #cambiar
 
 setup(
     name=package_name,
@@ -10,6 +12,10 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')), #cambiar
+        (os.path.join('share', package_name, 'config'), glob('config/*.png')), #cambiar
+        (os.path.join('share', package_name, 'config'), glob('config/*.rviz')), #cambiar
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')), #cambiar
     ],
     install_requires=['setuptools'],
     zip_safe=True,
